62e84314bd0cef71798a7f0462ed4557817b2607
farhanbr50 <farhanbr50@gmail.com>
2026-06-20 15:41:02 +0300

    Create __init__.py to define ALL_MODULES
    Initialize the module with a list of all available commands.

---
+++ ChandMusic/modules/__init__.py
@@ -0,0 +1,29 @@
+cat > ChandMusic/modules/__init__.py << 'EOF'
+# --------------------------------------------------------------------------------
+#  ✨ ChandMusic © 2026 ✨
+#  Developed by Chand ❤️
+#
+#  Unauthorized copying, editing, re-uploading or removing credits
+#  from this source code is strictly prohibited.
+# --------------------------------------------------------------------------------
+
+ALL_MODULES = [
+    "start",
+    "play",
+    "bot",
+    "pause",
+    "resume",
+    "skip",
+    "stop",
+    "seek",
+    "effects",
+    "autoplay",
+    "ping",
+    "stats",
+    "broadcast",
+    "block",
+    "callbacks",
+    "misc",
+    "nsfw",
+]
+EOF
